6bac1c82103b10d0b350453e7c59b4f543497c2d
dbauuexkac36 <dbauuexkac36@accrittermap.scm.azurewebsites.net>
2020-04-22 22:57:01 -0400

    added time travel tracker v.01

---
+++ src/timetravel.js
@@ -0,0 +1,112 @@
+
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+    
+    //set global variables for ajax json calls
+    var month;
+    var hour;
+    var hemi;
+
+
+
+$("#month").on('change',function(){
+        month = $(this).val();
+      });
+
+    $("#hour").on('change',function(){
+        hour = $(this).val();  
+      });
+
+    $("#hemisphere").on('change',function(){
+        hemi = $(this).val();   
+      });
+
+
+
+//bug time button ajax table load function      
+
+$('#bugtimeload').click(function(){
+console.log(month);
+console.log(hour);
+console.log(hemi);
+var invalid = "-";
+
+
+$.ajax({
+    url: hemi,
+    dataType: 'json',
+    type: 'get',
+    cache: false,
+    success: function (data) {
+      $(data.bugs).each(function (index, bug) {
+        if (bug.time.indexOf(hour) > -1 && bug.month.indexOf(month) > -1) {
+  
+          var tableRow =
+            '<tr><td>' +
+            bug.name +
+            '</td><td> <img src=' +
+            bug.image +
+            ' class="img-fluid" alt="responsive image"></td><td>' +
+            bug.price +
+            '</td><td>' +
+            bug.location +
+            '</td></tr>';
+          $(tableRow).appendTo('#timeloadbug tbody');
+          
+        }
+  
+      }); 
+    },
+  });
+
+});
+
+
+//fish time button ajax table load funtion
+
+$('#fishtimeload').click(function(){
+$.ajax({
+    url: hemi,
+    dataType: 'json',
+    type: 'get',
+    cache: false,
+    success: function (data) {
+      $(data.fish).each(function (index, fish) {
+        if (fish.time.indexOf(hour) > -1 && fish.month.indexOf(month) > -1) {
+  
+          var tableRow =
+            '<tr><td>' +
+            fish.name +
+            '</td><td> <img src=' +
+            fish.image +
+            ' class="img-fluid" alt="responsive image"></td><td>' +
+            fish.price +
+            '</td><td>' +
+            fish.location +
+            '</td><td>' +
+            fish.shadowsize +
+            '</td></tr>';
+          $(tableRow).appendTo('#timeloadfish tbody');
+          
+        }
+  
+      }); 
+    },
+  });
+});
+
+
+
+
+  
+    
+  
+
+
+
+
+}); //document ready end tag
+
+
+ 
